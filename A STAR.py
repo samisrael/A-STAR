4267814563fc4f4c8361948e2240a8290d7a32ba
SamIsrael <samisrael0407@gmail.com>
2025-04-28 22:08:13 +0530

    final

--- A STAR.py
+++ A STAR.py
@@ -1,119 +1,77 @@
-#!/usr/bin/env python
-# coding: utf-8
+import math
 
-# In[ ]:
+class Node:
+    def __init__(self, position, parent=None):
+        self.position = position
+        self.parent = parent
+        self.g = 0
+        self.h = 0
+        self.f = 0
 
+    def __eq__(self, other):
+        return self.position == other.position
 
-from collections import defaultdict
-H_dist ={}
-def aStarAlgo(start_node, stop_node):
-    open_set = set(start_node)
-    closed_set = set()
-    g = {}               #store distance from starting node
-    parents = {}         # parents contains an adjacency map of all nodes
-    #distance of starting node from itself is zero
-    g[start_node] = 0
-    #start_node is root node i.e it has no parent nodes
-    #so start_node is set to its own parent node
-    parents[start_node] = start_node
-    while len(open_set) > 0:
-        n = None
-        #node with lowest f() is found
-        for v in open_set:
-            if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
-                n = v
-        if n == stop_node or Graph_nodes[n] == None:
-            pass
-        else:
-            for (m, weight) in get_neighbors(n):
-                #nodes 'm' not in first and last set are added to first
-                #n is set its parent
-                if m not in open_set and m not in closed_set:
-                    open_set.add(m)
-                    parents[m] = n
-                    g[m] = g[n] + weight
-                #for each node m,compare its distance from start i.e g(m) to the
-                #from start through n node
-                else:
-                    if g[m] > g[n] + weight:
-                        #update g(m)
-                        g[m] = g[n] + weight
-                        #change parent of m to n
-                        parents[m] = n
-                        #if m in closed set,remove and add to open
-                        if m in closed_set:
-                            closed_set.remove(m)
-                            open_set.add(m)
-        if n == None:
-            TYPE THE CODE HERE TO PRINT AS Path does not exist!
-            
-            return None
-        
-        # if the current node is the stop_node
-        # then we begin reconstructin the path from it to the start_node
-        if n == stop_node:
+# Manhattan heuristic function
+def heuristic(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def astar_fixed():
+    grid = [
+        [0, 0, 0, 0],
+        [1, 1, 0, 1],
+        [0, 0, 0, 0],
+        [0, 1, 1, 0]
+    ]
+
+    start = (0, 0)
+    goal = (3, 3)
+
+    open_list = []
+    closed_list = []
+
+    start_node = Node(start)
+    goal_node = Node(goal)
+
+    open_list.append(start_node)
+
+    while open_list:
+        current_node = min(open_list, key=lambda node: node.f)
+        open_list.remove(current_node)
+
+        if current_node == goal_node:
             path = []
-            while parents[n] != n:
-                path.append(n)
-                n = parents[n]
-            path.append(start_node)
-            path.reverse()
-            print('Path found: {}'.format(path))
-            return path
-        # remove n from the open_list, and add it to closed_list
-        # because all of his neighbors were inspected
-        open_set.remove(n)
-        closed_set.add(n)
-    print('Path does not exist!')
+            while current_node:
+                path.append(current_node.position)
+                current_node = current_node.parent
+            return path[::-1]
+
+        neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0),
+                     (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+        for move in neighbors:
+            node_position = (current_node.position[0] + move[0],
+                             current_node.position[1] + move[1])
+
+            if (0 <= node_position[0] < len(grid)) and (0 <= node_position[1] < len(grid[0])):
+                if grid[node_position[0]][node_position[1]] == 0:
+                    successor = Node(node_position, current_node)
+
+                    successor.g = current_node.g + 1
+                    successor.h = heuristic(successor.position, goal_node.position)
+                    successor.f = successor.g + successor.h
+
+                    if any(open_node for open_node in open_list if successor == open_node and successor.f >= open_node.f):
+                        continue
+                    if any(closed_node for closed_node in closed_list if successor == closed_node and successor.f >= closed_node.f):
+                        continue
+
+                    open_list.append(successor)
+
+        closed_list.append(current_node)
+
     return None
 
-#define fuction to return neighbor and its distance
-#from the passed node
-def get_neighbors(v):
-       """
-    Retrieves a value from the Graph_nodes dictionary based on the provided key.
-
-    Parameters:
-    v (hashable): The key used to look up the value in the Graph_nodes dictionary.
-    Graph_nodes (dict): A dictionary where the keys are nodes and the values are associated data.
-
-    Returns:
-    The value associated with the key `v` in the Graph_nodes dictionary if the key exists; 
-    otherwise, returns None.
-    TYPE THE CODE HERE BY REFERRING TO THE ABOVE DESCRIPTION
-    """
-def heuristic(n):
-    return H_dist[n]
-
-
-#Describe your graph here
-'''Graph_nodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('A', 6), ('C', 3), ('D', 2)],
-    'C': [('B', 3), ('D', 1), ('E', 5)],
-    'D': [('B', 2), ('C', 1), ('E', 8)],
-    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-    'F': [('A', 3), ('G', 1), ('H', 7)],
-    'G': [('F', 1), ('I', 3)],
-    'H': [('F', 7), ('I', 2)],
-    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
-}
-'''
-graph = defaultdict(list)
-n,e = map(int,input().split())
-for i in range(e):
-    u,v,cost = map(str,input().split())
-    t=(v,float(cost))
-    graph[u].append(t)
-    t1=(u,float(cost))
-    graph[v].append(t1)
-for i in range(n):
-    node,h=map(str,input().split())
-    H_dist[node]=float(h)
-print(H_dist)
-
-   
-Graph_nodes=graph
-print(graph)
-aStarAlgo('S', 'G')
-
+# Run it!
+if __name__ == "__main__":
+    path = astar_fixed()
+    print("Found Path:", path)
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 <p>To ImplementA * Search algorithm for a Graph using Python 3.</p>
 <H3>Algorithm:</H3>
 
-``````
+
 // A* Search Algorithm
 1.  Initialize the open list
 2.  Initialize the closed list
@@ -48,7 +48,88 @@
     e) push q on the closed list
     end (while loop)
 
-``````
+<H3>Program</H3>
+
+```python
+import math
+
+class Node:
+    def __init__(self, position, parent=None):
+        self.position = position
+        self.parent = parent
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+# Manhattan heuristic function
+def heuristic(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def astar_fixed():
+    grid = [
+        [0, 0, 0, 0],
+        [1, 1, 0, 1],
+        [0, 0, 0, 0],
+        [0, 1, 1, 0]
+    ]
+
+    start = (0, 0)
+    goal = (3, 3)
+
+    open_list = []
+    closed_list = []
+
+    start_node = Node(start)
+    goal_node = Node(goal)
+
+    open_list.append(start_node)
+
+    while open_list:
+        current_node = min(open_list, key=lambda node: node.f)
+        open_list.remove(current_node)
+
+        if current_node == goal_node:
+            path = []
+            while current_node:
+                path.append(current_node.position)
+                current_node = current_node.parent
+            return path[::-1]
+
+        neighbors = [(0, -1), (0, 1), (-1, 0), (1, 0),
+                     (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+        for move in neighbors:
+            node_position = (current_node.position[0] + move[0],
+                             current_node.position[1] + move[1])
+
+            if (0 <= node_position[0] < len(grid)) and (0 <= node_position[1] < len(grid[0])):
+                if grid[node_position[0]][node_position[1]] == 0:
+                    successor = Node(node_position, current_node)
+
+                    successor.g = current_node.g + 1
+                    successor.h = heuristic(successor.position, goal_node.position)
+                    successor.f = successor.g + successor.h
+
+                    if any(open_node for open_node in open_list if successor == open_node and successor.f >= open_node.f):
+                        continue
+                    if any(closed_node for closed_node in closed_list if successor == closed_node and successor.f >= closed_node.f):
+                        continue
+
+                    open_list.append(successor)
+
+        closed_list.append(current_node)
+
+    return None
+
+# Run it!
+if __name__ == "__main__":
+    path = astar_fixed()
+    print("Found Path:", path)
+
+```
 
 <hr>
 <h2>Sample Graph I</h2>
@@ -117,3 +198,7 @@ G 0 <br>
 <h2>Sample Output</h2>
 <hr>
 Path found: ['A', 'E', 'D', 'G']
+
+
+<h3>Result</h3>
+Implementing A * Search algorithm for a Graph using Python 3. is executed successfully.
